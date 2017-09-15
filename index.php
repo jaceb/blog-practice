42ab1ed310c5ec070cdbcd25181fdb298cb04690
Jaceb <bri08006@byui.edu>
2017-09-14 22:27:18 -0600

    corrected config and index

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-function_autoload($class) {
+<?php
 
-  $class = strtolower($class);
+try {
 
-  $classpath = 'classes/class.'.$class . 'php';
-  if ( file_exists($classpath)) {
-    require_once $classpath;
-  }
+    $stmt = $db->query('SELECT postID, postTitle, postDesc, postDate FROM blog_posts ORDER BY postID DESC');
+    while($row = $stmt->fetch()){
 
-  $classpath = '../classes/class.'.$class . '.php';
-  if( file_exists($classpath)) {
-    require_once $classpath;
-  }
+      echo '<div>';
+          echo '<h1><a href="viewpost.php?id='$row['postID'].'">'.$row['postTitle'].'</a></h1>';
+          echo '<p>Posted on '.date('js M Y H:i:s', strtotime($row['postDate']))'</p>';
+          echo '<p>'.$row['postDesc'].'</p>';
+          echo '<p><a href="viewpost.php?id='.$row['postID'].'">Read More</a</p>';
+          echo '</div>';
+    }
+
+
+} catch (PDOException $e) {
+  echo $e->getMessage();
 }
 
-$user = new User($db);
+
+
+
+
+ ?>
